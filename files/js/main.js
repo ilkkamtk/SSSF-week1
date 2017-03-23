cff97b51ff9f206096f6aa76003fa03197533ca4
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2017-03-24 00:05:50 +0200

    update

--- files/js/main.js
+++ files/js/main.js
@@ -1,64 +1,77 @@
 'use strict';
 
-const spyArray = [
-    {
-        "id": 12,
-        "time": "2017-03-02 22:55",
-        "category": "Wife",
-        "title": "Title 1",
-        "details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.",
-        "coordinates": {
-            "lat": 60.2196781,
-            "lng": 24.8079786
-        },
-        "thumbnail": "http://placekitten.com/320/300",
-        "image": "http://placekitten.com/768/720",
-        "original": "http://placekitten.com/2048/1920"
-    },
-    {
-        "id": 15,
-        "time": "2017-03-01 19:23",
-        "category": "Wife",
-        "title": "Title 2",
-        "details": "Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ",
-        "coordinates": {"lat": 60.3196781, "lng": 24.9079786},
-        "thumbnail": "http://placekitten.com/321/300",
-        "image": "http://placekitten.com/770/720",
-        "original": "http://placekitten.com/2041/1920"
-    },
-    {
-        "id": 34,
-        "time": "2017-12-04 09:45",
-        "category": "Girlfriend",
-        "title": "Title 3",
-        "details": "Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ",
-        "coordinates": {"lat": 60.3196781, "lng": 24.9079786},
-        "thumbnail": "http://placekitten.com/319/300",
-        "image": "http://placekitten.com/769/720",
-        "original": "http://placekitten.com/2039/1920"
+let originalData = null;
+
+document.querySelector('#reset-button').addEventListener('click', () => {
+    update(originalData);
+});
+
+const createCard = (image, title, text) => {
+    return `<img class="card-img-top" src="${image}" alt="">
+            <div class="card-block">
+                <h3 class="card-title">${title}</h3>
+                <p class="card-text">${text}</p>
+            </div>
+            <div class="card-footer">
+                <p><button class="btn btn-primary">View</button></p>
+            </div>`;
+};
+
+const categoryButtons = (items) => {
+    items = removeDuplicates(items, 'category');
+    console.log(items);
+    document.querySelector('#categories').innerHTML = '';
+    for (let item of items) {
+        const button = document.createElement('button');
+        button.class = 'btn btn-secondary';
+        button.innerText = item.category;
+        document.querySelector('#categories').appendChild(button);
+        button.addEventListener('click', () => {
+            sortItems(originalData, item.category);
+        });
     }
-];
+};
 
+const sortItems = (items, rule) => {
+    const newItems = items.filter(item => item.category === rule);
+    // console.log(newItems);
+    update(newItems);
+};
 
-for (let item of spyArray) {
-    console.log(item);
-    const article = document.createElement('article');
-    article.setAttribute('class' , 'card');
+const getData = () => {
+    fetch('files/data.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(items => {
+            originalData = items;
+            update(items);
+        });
 
-    article.innerHTML = `
-                        <img class="card-img-top" src="${item.thumbnail}" alt="">
-                        <div class="card-block">
-                            <h3 class="card-title">${item.title}</h3>
-                            <p class="card-text">${item.details}</p>
-                        </div>
-                        <div class="card-footer">
-                            <p><button class="btn btn-primary">View</button></p>
-                        </div>
-                        `;
-    article.addEventListener('click', (evt) => {
-        document.querySelector('.modal-body img').src = item.image;
-        document.querySelector('.modal-title').innerHTML = item.title;
-        $('#myModal').modal('show');
+};
+
+const removeDuplicates = (myArr, prop) => {
+    return myArr.filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
     });
-    document.querySelector('.card-deck').appendChild(article);
 }
+
+const update = (items) => {
+    categoryButtons(items);
+    document.querySelector('.card-deck').innerHTML = '';
+    for (let item of items) {
+        // console.log(item);
+        const article = document.createElement('article');
+        article.setAttribute('class', 'card');
+
+        article.innerHTML = createCard(item.thumbnail, item.title, item.details);
+        article.addEventListener('click', () => {
+            document.querySelector('.modal-body img').src = item.image;
+            document.querySelector('.modal-title').innerHTML = item.title;
+            $('#myModal').modal('show');
+        });
+        document.querySelector('.card-deck').appendChild(article);
+    }
+};
+
+getData();
