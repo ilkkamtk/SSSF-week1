c354bd88b8354a30d4468f71acbc02330acc5f4d
Ilkka Kylmäniemi <ilkkamtk@iMac.local>
2017-04-02 15:11:04 +0300

    simplifying

--- files/js/main.js
+++ files/js/main.js
@@ -1,16 +1,23 @@
 'use strict';
 
 let originalData = null;
+let map = null;
+let marker = null;
 
 document.querySelector('#reset-button').addEventListener('click', () => {
     update(originalData);
 });
 
-const createCard = (image, title, text) => {
+const createCard = (image, title, texts) => {
+    let text = '';
+    for (let t of texts){
+        text += `<p class="card-text">${t}</p>`;
+    }
+
     return `<img class="card-img-top" src="${image}" alt="">
             <div class="card-block">
                 <h3 class="card-title">${title}</h3>
-                <p class="card-text">${text}</p>
+                ${text}                
             </div>
             <div class="card-footer">
                 <p><button class="btn btn-primary">View</button></p>
@@ -39,7 +46,7 @@ const sortItems = (items, rule) => {
 };
 
 const getData = () => {
-    fetch('files/data.json')
+    fetch('data.json')
         .then(response => {
             return response.json();
         })
@@ -54,7 +61,7 @@ const removeDuplicates = (myArr, prop) => {
     return myArr.filter((obj, pos, arr) => {
         return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
     });
-}
+};
 
 const update = (items) => {
     categoryButtons(items);
@@ -63,15 +70,43 @@ const update = (items) => {
         // console.log(item);
         const article = document.createElement('article');
         article.setAttribute('class', 'card');
-
-        article.innerHTML = createCard(item.thumbnail, item.title, item.details);
+        const time = moment(item.time);
+        article.innerHTML = createCard(item.thumbnail, item.title, ['<small>'+time.format('dddd, MMMM Do YYYY, HH:mm')+'</small>', item.details]);
         article.addEventListener('click', () => {
             document.querySelector('.modal-body img').src = item.image;
             document.querySelector('.modal-title').innerHTML = item.title;
-            $('#myModal').modal('show');
+            resetMap(item);
+            document.querySelector('#map').addEventListener('transitionend', () => {
+                resetMap(item);
+            });
+            const myModal = $('#myModal');
+            myModal.on('shown.bs.modal', () => {
+                resetMap(item);
+            });
+            myModal.modal('show');
         });
         document.querySelector('.card-deck').appendChild(article);
     }
 };
 
-getData();
+const initMap = () => {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11
+    });
+    marker = new google.maps.Marker({
+        map: map
+    });
+    getData();
+};
+
+const resetMap = (item) => {
+    const coords = item.coordinates;
+    console.log(coords);
+    google.maps.event.trigger(map, "resize");
+    map.panTo(coords);
+    marker.setOptions({
+        position: coords
+    });
+};
+
+initMap();
